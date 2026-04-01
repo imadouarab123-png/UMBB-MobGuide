557dc2bc010da77e79c2efcc2f338d67eb0e277b
Yanis Amrani <example@gmail.com>
2026-04-02 00:09:45 +0100

    first commit

--- app/src/main/java/com/example/umbbmobguide/AboutActivity.java
+++ app/src/main/java/com/example/umbbmobguide/AboutActivity.java
@@ -1,4 +1,15 @@
 package com.example.umbbmobguide;
 
-public class AboutActivity {
+
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AboutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+    }
 }
--- app/src/main/java/com/example/umbbmobguide/FacultiesActivity.java
+++ app/src/main/java/com/example/umbbmobguide/FacultiesActivity.java
@@ -1,4 +1,15 @@
 package com.example.umbbmobguide;
 
-public class FacultiesActivity {
+
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class FacultiesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_faculties);
+    }
 }
